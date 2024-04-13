e0b8e2d5d37f44f679814234370c16942f583ee2
Rakeshkanth77 <124147815+Rakeshkanth77@users.noreply.github.com>
2024-04-13 19:37:50 +0100

    all files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lingo assistant",
+    version="0.0.1",
+    author="Rakesh kanth",
+    author_email="rakeshkanth77@gmail.com",
+    packages=find_packages()
+)
